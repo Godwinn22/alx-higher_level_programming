78fc051baa4932ed8a8a9d22ec2b1a37d25b1f83
godwinn22 <godwinohazulike@gmail.com>
2024-01-03 21:09:29 +0100

    Solved task 1. The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 last_dig = abs(number) % 10
 
 if last_dig > 5:
-    print("Last digit of ", number, "is", last_dig, "and is greater than 5")
+    print("Last digit of", number, "is", last_dig, "and is greater than 5")
 elif last_dig == 0:
-    print("Last digit of ", number, "is", last_dig, "and is 0")
+    print("Last digit of", number, "is", last_dig, "and is 0")
 elif last_dig < 6 and last_dig != 0:
-    print("Last digit of ", number, "is", "and is less than 6 and not 0")
+    print("Last digit of", number, "is", "and is less than 6 and not 0")
