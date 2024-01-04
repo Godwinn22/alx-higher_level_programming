e07e530a22e20576205ed0a43a4641d662378cce
godwinn22 <godwinohazulike@gmail.com>
2024-01-04 01:26:00 +0100

    Solved task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_i = ''
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            upper_i += chr(ord(i) - 32)
+        else:
+            upper_i += i
+    print(upper_i)
