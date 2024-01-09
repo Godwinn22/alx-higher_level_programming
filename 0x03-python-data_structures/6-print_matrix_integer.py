b194b31e5b5f9263a66791e9f3f3f778cd7ada47
godwinn22 <godwinohazulike@gmail.com>
2024-01-09 12:25:39 +0100

    Solved task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for val, j in enumerate(i):
             if val < len(i) - 1:
-                print("{}".format(j), end=" ")
+                print("{:d}".format(j), end=" ")
             else:
-                print("{}".format(j), end="")
+                print("{:d}".format(j), end="")
         print()
