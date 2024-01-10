0f82cab084ccfaf47fac7aac4d7860fe96d326ad
godwinn22 <godwinohazulike@gmail.com>
2024-01-10 03:58:10 +0100

    Solved task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    else:
+        length = len(sentence)
+        first = sentence[0]
+
+        result = (length, first)
+        return result
