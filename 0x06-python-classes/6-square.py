dce2da9a82a0aefc236888b34556ed09763c9405
godwinn22 <godwinohazulike@gmail.com>
2024-01-30 22:51:50 +0100

    Solved task 6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
         Attributes:
             __size: The size of the square
         """
-        if not isinstance(value, tuple) and len(value) != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
