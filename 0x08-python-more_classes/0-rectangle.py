6d66711205f50486901e10d3d5e2ed71e28dba76
godwinn22 <godwinohazulike@gmail.com>
2024-01-31 16:32:55 +0100

    Solved task 1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This is an empty class Rectangle that defines a rectangle"""
+
+
 class Rectangle:
     """This is an empty class Rectangle that defines a rectangle"""
     pass
