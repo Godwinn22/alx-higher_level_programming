0078cb4cf224b3190abf142ec8c4115c62f8adff
godwinn22 <godwinohazulike@gmail.com>
2024-02-03 14:05:53 +0100

    Solved task 2. Say my name

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -4,4 +4,12 @@ say_my_name = __import__('3-say_my_name').say_my_name
 say_my_name("John", "Smith")
 say_my_name("Walter", "White")
 say_my_name("Bob")
-say_my_name(12, "White")
+try:
+    say_my_name(5, 5)
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
+try:
+    say_my_name("john", 6)
+except Exception as e:
+    print(e)
