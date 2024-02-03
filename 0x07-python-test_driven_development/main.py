52ceda8e0dd4a4d58249718ee5cc1195e507dd8f
godwinn22 <godwinohazulike@gmail.com>
2024-02-03 14:11:52 +0100

    Solved task 2. Say my name

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -3,6 +3,7 @@ say_my_name = __import__('3-say_my_name').say_my_name
 
 say_my_name("John", "Smith")
 say_my_name("Walter", "White")
+say_my_name("Walter", "")
 say_my_name("Bob")
 try:
     say_my_name(5, 5)
