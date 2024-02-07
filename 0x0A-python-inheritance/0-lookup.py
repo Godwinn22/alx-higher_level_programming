27a4c9e0003c0c0b6e4655799e585a6edd84758f
godwinn22 <godwinohazulike@gmail.com>
2024-02-07 14:54:38 +0100

    Solvesd task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a function that returns the list of available
+attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """This is a function that returns the list of available
+    attributes and methods of an object
+    """
+    return dir(obj)
