b5cd4b656bc8907a841e7fdbf90e584515d9c34c
godwinn22 <godwinohazulike@gmail.com>
2024-02-07 16:55:50 +0100

    Solved task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,6 @@ class BaseGeometry:
     def area(self):
         """A function that raises an Exception"""
         raise Exception("area() is not implemented")
-
     def integer_validator(self, name, value):
         """Another method that validates value"""
         if not isinstance(value, int):
