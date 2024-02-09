bc634f3cea8b6aa31a2a1fc5a18ea4df6f9f40b2
godwinn22 <godwinohazulike@gmail.com>
2024-02-09 18:32:43 +0100

    Solved task 3. To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,4 +4,9 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    This is a function returns the JSON representation of an object (string)
+    Args:
+        my_obj (any): the object we are to serialize
+    """
     return json.dumps(my_obj)
