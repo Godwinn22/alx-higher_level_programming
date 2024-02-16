54a3494b03f836c388e36d39a48be87597c99616
godwinn22 <godwinohazulike@gmail.com>
2024-02-16 21:15:09 +0100

    Solved task 2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,17 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Defines a class that inherits form BAse"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
