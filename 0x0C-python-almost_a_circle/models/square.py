1a2f5d27309a4237493d3376b14aafad4e385bd5
godwinn22 <godwinohazulike@gmail.com>
2024-03-02 13:08:00 +0100

    Solved task 10. And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """Defines a class Square that inherits from Rectangle"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """
     A class called square that inherits from the rectangle class.
@@ -15,10 +16,11 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
         self.size = size
-    
+
     def __str__(self):
         """
         return [Square] (<id>) <x>/<y> - <size> -
         in our case, width or height
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.size}" 
+        return f"[{self.__class__.__name__}]\
+            ({self.id}) {self.x}/{self.y} - {self.size}"
