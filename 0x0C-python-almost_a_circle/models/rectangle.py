d3cc8a06c3ec5b860db99528d6b55affe72a46f4
godwinn22 <godwinohazulike@gmail.com>
2024-03-02 12:59:58 +0100

    Solved task 10. And now, the Square!

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,6 @@
 """Defines a class Rectangle that inherits from Base"""
 from models.base import Base
 
-
 class Rectangle(Base):
     """Defines a class Rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
