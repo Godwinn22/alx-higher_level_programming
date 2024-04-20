3d4c81d528b17ec2b612b6af7863ff6ec4a28e9d
godwinn22 <godwinohazulike@gmail.com>
2024-04-20 12:10:12 +0100

    Solved task V

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This is a script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+from relationship_state import Base, State
+from relationship_city import City
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, passwd, db_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state_col = session.query(State).outerjoin(City).order_by(
+        State.id, City.id).all()
+
+    for state in state_col:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
