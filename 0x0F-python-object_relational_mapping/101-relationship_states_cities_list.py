5ad6515223fb235a413bb9224ee58f358f9144c3
godwinn22 <godwinohazulike@gmail.com>
2024-04-20 12:10:59 +0100

    Solved task a script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This is a script that lists all State objects
-from the database hbtn_0e_6_usa
+This is a script that lists all State objects, and corresponding City
+objects, contained in the database hbtn_0e_101_usa
 """
 from relationship_state import Base, State
 from relationship_city import City
