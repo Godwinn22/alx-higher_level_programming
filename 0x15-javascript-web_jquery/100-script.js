22c269cf84d7f0ba77bd65f470d2d1b03f3b0612
godwinn22 <godwinohazulike@gmail.com>
2024-06-03 22:14:23 +0100

    Completed all advanced task

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,9 @@
+// This is a JavaScript script that updates the text color of the
+// <header> element to red (#FF0000)
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header');
+    header.style.color = '#FF0000';
+  });
+})();
